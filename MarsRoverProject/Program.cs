4d362ba76d3cd2386e22dd6057cddb4924b8b57e
emine.bakir <emine.bakir@API065>
2021-12-28 10:21:19 +0300

    Add project files.

---
+++ MarsRoverProject/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MarsRover
+{
+
+    static int m;
+    static int n;
+    static void Main(string[] args)
+    {
+
+        m = 5;
+        n = 5;
+        Position currentPosition = new Position();
+        currentPosition.x = 1;
+        currentPosition.y = 2;
+        currentPosition.dir = "N";
+
+        String command = "LMLMLMLMM";
+
+        foreach (char c in command)
+        {
+            if (c == 'L')
+            {
+                if (currentPosition.dir == "N")
+                    currentPosition.dir = "W";
+                else if (currentPosition.dir == "W")
+                    currentPosition.dir = "S";
+                else if (currentPosition.dir == "S")
+                    currentPosition.dir = "E";
+                else if (currentPosition.dir == "E")
+                    currentPosition.dir = "N";  
+
+            }
+
+            if (c == 'M')
+            {
+                if (currentPosition.dir == "N")
+                    currentPosition.y++;
+                else if (currentPosition.dir == "W")
+                    currentPosition.x--;
+                else if (currentPosition.dir == "S")
+                    currentPosition.y--;
+                else if (currentPosition.dir == "E")
+                    currentPosition.x++;
+
+            }
+
+        }
+
+        Console.WriteLine(currentPosition.x + " " + currentPosition.y + " " + currentPosition.dir);
+
+
+    }
+
+
+    class Position
+    {
+        public int x;
+        public int y;
+        public String dir;
+
+    }
+
+}
+
+
+
+
+
